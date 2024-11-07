65090641e442903df7d94bef788a8d05d2cebf19
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-11-08 03:40:33 +0500

    create a folder, create files of html and js, and understand all about arrow function

---
+++ Arrow Function/script.js
@@ -0,0 +1,40 @@
+// arrow function
+// const addTwoNums = (num1, num2) => {
+//  return num1 + num2;
+// }
+
+// console.log(addTwoNums(2, 3))
+
+
+// Or
+
+
+// Short form
+// const addTwoNums = (num1, num2) => num1 + num2;
+// console.log(addTwoNums(2, 3))
+
+
+// or 
+
+// const addTwoNums = (num1, num2) => (num1 + num2);
+// console.log(addTwoNums(2, 3))
+ 
+// ================================
+// ================================
+
+
+
+// let greet = () => console.log('Mohib Ullah');
+// greet();
+
+
+// const regin = (add) => (console.log(`Hello ${add}`)) 
+// regin('afridi')
+
+
+
+// Arrow function with setTimeout method
+// setTimeout( () => console.log('Welcome! Mohib.'), 2000)
+
+
+
